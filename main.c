4f12abc13df43fc66f7c183cef271acd88178406
Timon Kelsie <tkelsie@student.21-school.ru>
2019-07-24 17:51:16 +0300

    a

--- main.c
+++ main.c
@@ -6,13 +6,12 @@
 /*   By: tkelsie <tkelsie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 14:16:09 by tkelsie           #+#    #+#             */
-/*   Updated: 2019/07/24 17:10:54 by tkelsie          ###   ########.fr       */
+/*   Updated: 2019/07/24 17:50:58 by tkelsie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "fdf.h"
-#include <stdio.h>
 
 int		main(int ac, char **av)
 {
@@ -25,10 +24,8 @@ int		main(int ac, char **av)
 	fdf_read(&megastruct);
 	megastruct.img = mlx_new_image(megastruct.discriptor, WIDTH, HIGHT);
 	megastruct.data_adress = (unsigned int *)mlx_get_data_addr(megastruct.img, &megastruct.bits_per_pixel, &megastruct.size_line, &megastruct.endian);
-	//printf("%d", megastruct.str_quantity);
-	//printf("%s", megastruct.data_adress);
 	draw(&megastruct);
-	mlx_put_image_to_window(megastruct.discriptor, megastruct.win, megastruct.img, 10, 10);
+	mlx_put_image_to_window(megastruct.discriptor, megastruct.win, megastruct.img, 0, 0);
 	mlx_hook(megastruct.win, 2, 0, keyboard, 0);
 	mlx_loop(megastruct.discriptor);
 	zbs();
