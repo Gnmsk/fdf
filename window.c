4bc351e4d269611935fb277d20113112d8d97a8c
Timon Kelsie <tkelsie@student.21-school.ru>
2019-07-26 22:27:31 +0300

    a

--- window.c
+++ window.c
@@ -6,7 +6,7 @@
 /*   By: tkelsie <tkelsie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 22:03:08 by tkelsie           #+#    #+#             */
-/*   Updated: 2019/07/26 22:24:01 by tkelsie          ###   ########.fr       */
+/*   Updated: 2019/07/26 22:27:28 by tkelsie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,27 @@ void			interface(t_mega *megastructura)
 {
 	mlx_string_put(megastructura->discriptor, megastructura->win, 0, 0, \
 	0xFF, "left-right-up-down for move on x/y/z axis");
+}
+
+void	put_pixel(int x, int y, int color, t_mega *megastruct)
+{
+	megastruct->data_adress[y * WIDTH + x] = color;
+}
+
+void	fill_backgound(int color, t_mega *megastruct)
+{
+	int				x;
+	int				y;
+
+	y = 0;
+	while (y < HIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			megastruct->data_adress[y * WIDTH + x] = color;
+			x++;
+		}
+		y++;
+	}
 }
