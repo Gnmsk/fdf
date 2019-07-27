b5fb2ad9b89aacde549003214474462728c1352e
Timon Kelsie <tkelsie@student.21-school.ru>
2019-07-27 19:22:03 +0300

    a

--- srcs/fdf.h
+++ srcs/fdf.h
@@ -6,7 +6,7 @@
 /*   By: tkelsie <tkelsie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 14:18:33 by tkelsie           #+#    #+#             */
-/*   Updated: 2019/07/27 19:11:56 by tkelsie          ###   ########.fr       */
+/*   Updated: 2019/07/27 19:21:04 by tkelsie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,6 +116,9 @@ void				shift_xy(t_mega *megastruct, int step);
 void				shift(t_mega *megastructura);
 void				zoom(t_mega *megastruct, double zoom);
 void				centre(t_mega *megastruct);
+void				projection(t_mega *megastruct);
+void				movements(int key, t_mega *megastruct);
+void				zooming(int key, t_mega *megastruct);
 
 void				interface(t_mega *megastructura);
 void				display_map(t_mega *megastructura);
--- srcs/keyboard.c
+++ srcs/keyboard.c
@@ -6,11 +6,58 @@
 /*   By: tkelsie <tkelsie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 14:15:13 by tkelsie           #+#    #+#             */
-/*   Updated: 2019/07/27 19:13:08 by tkelsie          ###   ########.fr       */
+/*   Updated: 2019/07/27 19:21:57 by tkelsie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	projection(t_mega *megastruct)
+{
+	megastruct->iso_coords = copy_structure(megastruct);
+	zoom(megastruct, megastruct->zoom);
+	if (megastruct->proj == 1)
+		megastruct->proj = 0;
+	else if (megastruct->proj == 0)
+		rotation(megastruct);
+	shift(megastruct);
+	centre(megastruct);
+}
+
+void	movements(int key, t_mega *megastruct)
+{
+	if (key == 83)
+		megastruct->alpha -= 0.05;
+	if (key == 84)
+		megastruct->alpha += 0.05;
+	if (key == 85)
+		megastruct->beta -= 0.05;
+	if (key == 86)
+		megastruct->beta += 0.05;
+	if (key == 87)
+		megastruct->gamma -= 0.05;
+	if (key == 88)
+		megastruct->gamma += 0.05;
+	megastruct->iso_coords = copy_structure(megastruct);
+	zoom(megastruct, megastruct->zoom);
+	rotation(megastruct);
+	shift(megastruct);
+	centre(megastruct);
+}
+
+void	zooming(int key, t_mega *megastruct)
+{
+	megastruct->iso_coords = copy_structure(megastruct);
+	if (key == 69)
+		megastruct->zoom += 3;
+	else if (key == 78)
+		if (megastruct->zoom > 3)
+			megastruct->zoom -= 3;
+	zoom(megastruct, megastruct->zoom);
+	if (megastruct->proj == 1)
+		rotation(megastruct);
+	centre(megastruct);
+}
 
 int	keyboard(int key, t_mega *megastruct)
 {
@@ -19,49 +66,11 @@ int	keyboard(int key, t_mega *megastruct)
 	if (key >= 123 && key <= 126)
 		shift_xy(megastruct, key);
 	if (key == 69 || key == 78)
-	{
-		megastruct->iso_coords = copy_structure(megastruct);
-		if (key == 69)
-			megastruct->zoom += 3;
-		else if (key == 78)
-			if (megastruct->zoom > 3)
-				megastruct->zoom -= 3;
-		zoom(megastruct, megastruct->zoom);
-		if (megastruct->proj == 1)
-			rotation(megastruct);
-		centre(megastruct);
-	}
+		zooming(key, megastruct);
 	if (key == 49)
-	{
-		megastruct->iso_coords = copy_structure(megastruct);
-		zoom(megastruct, megastruct->zoom);
-		if (megastruct->proj == 1)
-			megastruct->proj = 0;
-		else if (megastruct->proj == 0)
-			rotation(megastruct);
-		shift(megastruct);
-		centre(megastruct);
-	}
+		projection(megastruct);
 	if (key >= 83 && key <= 88)
-	{
-		if (key == 83)
-			megastruct->alpha -= 0.05;
-		if (key == 84)
-			megastruct->alpha += 0.05;
-		if (key == 85)
-			megastruct->beta -= 0.05;
-		if (key == 86)
-			megastruct->beta += 0.05;
-		if (key == 87)
-			megastruct->gamma -= 0.05;
-		if (key == 88)
-			megastruct->gamma += 0.05;
-		megastruct->iso_coords = copy_structure(megastruct);
-		zoom(megastruct, megastruct->zoom);
-		rotation(megastruct);
-		shift(megastruct);
-		centre(megastruct);
-	}
+		movements(key, megastruct);
 	if (key == 82)
 		default_state(megastruct);
 	display_map(megastruct);
